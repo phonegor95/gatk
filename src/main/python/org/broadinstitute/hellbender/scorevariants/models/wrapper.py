17c2e0172b21256785b6de873b6898fc2fd24aaf
hong <phoenixhung2@gmail.com>
2025-01-20 13:53:51 +0000

    :multiGPU NVscore

--- src/main/python/org/broadinstitute/hellbender/scorevariants/models/wrapper.py
+++ src/main/python/org/broadinstitute/hellbender/scorevariants/models/wrapper.py
@@ -1,7 +1,9 @@
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from scorevariants.models.common import predictions_to_score
-import numpy as np
+from torch import distributed as dist
+from torch.distributed import all_gather_object, get_world_size
+from natsort import natsorted
 
 class LightningWrapper(pl.LightningModule):
     def __init__(self, model,
@@ -24,17 +26,41 @@ class LightningWrapper(pl.LightningModule):
         self.save_hyperparameters()
         self.test_results = []
         if tmp_file:
-            self.tmp_file = tmp_file 
+            self.tmp_file = open(tmp_file, 'w')
 
+    def _dedup(self, merged_results):
+        sorted_results = natsorted(merged_results, key=lambda x: (x['chrom'], x['pos'].item()))
+        
+        unique_results = []
+        previous_result = None
+
+        for result in sorted_results:
+            if previous_result and result['pos'].item() == previous_result['pos'].item() and result['alt'] == previous_result['alt']:
+                continue
+            unique_results.append(result)
+            previous_result = result
+
+        return unique_results
+    
     def forward(self, batch):
         outputs = self.model(batch)
         return outputs
 
     def on_test_end(self):
-        with open(self.tmp_file, "w") as f:
-            for result in self.test_results:
-                f.write('%s\t%d\t%s\t[%s]\t%.3f\n' % (
-                    result['chrom'], result['pos'], result['ref'], result['alt'], result['score']))
+        if dist.is_initialized():
+            dist.barrier()
+
+            all_results = [None] * dist.get_world_size()
+            dist.all_gather_object(all_results, self.test_results)
+            merged_results = sum(all_results, [])
+            unique_results = self._dedup(merged_results)    
+
+            if self.global_rank == 0 and self.tmp_file:
+                for result in unique_results:
+                    self.tmp_file.write('%s\t%d\t%s\t[%s]\t%.3f\n' % (
+                        result['chrom'], result['pos'], result['ref'], result['alt'], result['score']))
+        
+        self.tmp_file.close()
         
     def test_step(self, batch, batch_idx):
         predictions = self(batch)
