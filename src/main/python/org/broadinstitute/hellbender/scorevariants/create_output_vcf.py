27933e3f936759aa79418f2d365a17b5a290130c
phonegor95 <phoenixhung2@gmail.com>
2025-05-15 14:52:48 +0000

    write output vcf by cyvcf2

--- src/main/python/org/broadinstitute/hellbender/scorevariants/create_output_vcf.py
+++ src/main/python/org/broadinstitute/hellbender/scorevariants/create_output_vcf.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-from pysam import VariantFile
+from cyvcf2 import VCF, Writer
 import re
-import argparse
 import sys
 
 CONTIG_INDEX = 0;
@@ -11,29 +10,41 @@ REF_INDEX = 2;
 ALT_INDEX = 3;
 KEY_INDEX = 4;
 
-def create_output_vcf(vcf_in, scores_file, vcf_out, label):
-    variant_file = VariantFile(vcf_in)
-    variant_file.reset()
+def create_output_vcf(vcf_in, scores_file, vcf_out, label, n_gpus):
+    variant_file = VCF(vcf_in)
 
-    variant_file.header.info.add(id=label, number=1, type='Float', description='Log odds of being a true variant versus \
-                    being false under the trained Convolutional Neural Network')
-    header = variant_file.header.copy()
-    vcfWriter = VariantFile(vcf_out, 'w', header=header)
+    variant_file.add_info_to_header({'ID': label, 'Number': 1, 'Type': 'Float', 'Description': 'Log odds of being a true variant versus being false under the trained Convolutional Neural Network'})
+    w = Writer(vcf_out, variant_file)
 
-    with open(scores_file) as scoredVariants:
-        sv = next(scoredVariants)
-        for variant in variant_file:
+    score_handles = [open(score_file, 'r') for score_file in [f".{scores_file}.{i}.tmp" for i in range(n_gpus)]]
+    endofVCF = False
+
+    while True:
+        scoredVariants = [handle.readline() for handle in score_handles]
+        for i, sv in enumerate(scoredVariants):
+            try:
+                variant = next(variant_file)
+            except StopIteration:
+                endofVCF = True
+                break
             scoredVariant = sv.split('\t')
-            if variant.contig == scoredVariant[CONTIG_INDEX] and \
-               variant.pos == int(scoredVariant[POS_INDEX]) and \
-               variant.ref == scoredVariant[REF_INDEX] and \
-               ', '.join(variant.alts or []) == re.sub('[\[\]]', '', scoredVariant[ALT_INDEX]):
+            if variant.CHROM == scoredVariant[CONTIG_INDEX] and \
+               variant.POS == int(scoredVariant[POS_INDEX]) and \
+               variant.REF == scoredVariant[REF_INDEX] and \
+               ', '.join(variant.ALT or []) == re.sub('[\[\]]', '', scoredVariant[ALT_INDEX]):
 
                     if len(scoredVariant) > KEY_INDEX:
-                        variant.info.update({label: float(scoredVariant[KEY_INDEX])})
+                        variant.INFO["CNN_2D"] = float(scoredVariant[KEY_INDEX])
 
-                    vcfWriter.write(variant)
+                    w.write_record(variant)
 
-                    sv = next(scoredVariants, None)
             else:
-                sys.exit("Score file out of sync with original VCF. Score file has: " + sv + "\nBut VCF has: " + str(variant))
+                sys.exit(f"Score file {i} out of sync:\n"
+                         f"  Score: {sv}\n"
+                         f"  VCF:   {variant}\n")
+        if endofVCF:
+            break
+
+    for h in score_handles:
+        h.close()
+    w.close()
--- src/main/python/org/broadinstitute/hellbender/scorevariants/models/wrapper.py
+++ src/main/python/org/broadinstitute/hellbender/scorevariants/models/wrapper.py
@@ -3,7 +3,8 @@ import pytorch_lightning as pl
 from scorevariants.models.common import predictions_to_score
 from torch import distributed as dist
 from torch.distributed import all_gather_object, get_world_size
-from natsort import natsorted
+import sys
+import os
 
 class LightningWrapper(pl.LightningModule):
     def __init__(self, model,
@@ -25,41 +26,24 @@ class LightningWrapper(pl.LightningModule):
         # with model checkpointing by saving the model
         self.save_hyperparameters()
         self.test_results = []
-        if tmp_file:
-            self.tmp_file = open(tmp_file, 'w')
-
-    def _dedup(self, merged_results):
-        sorted_results = natsorted(merged_results, key=lambda x: (x['chrom'], x['pos'].item()))
-        
-        unique_results = []
-        previous_result = None
-
-        for result in sorted_results:
-            if previous_result and result['pos'].item() == previous_result['pos'].item() and result['alt'] == previous_result['alt']:
-                continue
-            unique_results.append(result)
-            previous_result = result
-
-        return unique_results
+        self.tmp_file = tmp_file
     
     def forward(self, batch):
         outputs = self.model(batch)
         return outputs
 
+    def on_test_start(self):
+        if self.tmp_file:
+            # Set the tmp_file after the distributed initialization
+            tmp_files = f".{self.tmp_file}.{self.global_rank}.tmp"
+            if os.path.exists(tmp_files):
+                os.remove(tmp_files)
+            self._tmp_fh = open(tmp_files, 'a')
+            sys.stdout.write(f"Rank {self.global_rank} is writing to {self.tmp_file}\n")
+
     def on_test_end(self):
         if dist.is_initialized():
             dist.barrier()
-
-            all_results = [None] * dist.get_world_size()
-            dist.all_gather_object(all_results, self.test_results)
-            merged_results = sum(all_results, [])
-            unique_results = self._dedup(merged_results)    
-
-            if self.global_rank == 0 and self.tmp_file:
-                for result in unique_results:
-                    self.tmp_file.write('%s\t%d\t%s\t[%s]\t%.3f\n' % (
-                        result['chrom'], result['pos'], result['ref'], result['alt'], result['score']))
-        
         self.tmp_file.close()
         
     def test_step(self, batch, batch_idx):
@@ -67,11 +51,9 @@ class LightningWrapper(pl.LightningModule):
         detached_predictions = predictions.detach()
         prob_predictions = F.softmax(detached_predictions)
         scores = predictions_to_score(prob_predictions.detach(), batch['type'])
-        for i in range(len(scores)):
-            self.test_results.append({
-                'chrom': batch['chrom'][i],
-                'pos': batch['pos'][i],
-                'ref': batch['ref'][i],
-                'alt': batch['alt'][i],
-                'score': scores[i]
-            })
+        for i, score in enumerate(scores):
+            chrom = batch['chrom'][i]
+            pos   = batch['pos'][i].cpu().item()
+            ref   = batch['ref'][i]
+            alt   = batch['alt'][i]
+            self._tmp_fh.write(f"{chrom}\t{pos}\t{ref}\t[{alt}]\t{score.cpu().item():.3f}\n")
